beb67de4508dbe987d78482d65363506e6852019
PC <PC@DESKTOP-EI5L27V>
2026-03-09 16:21:20 +0300

    добавлен new class

---
+++ newClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first3
+{
+    public class newClass
+    {
+        public string Name { get; private set; }
+        
+    }
+}
